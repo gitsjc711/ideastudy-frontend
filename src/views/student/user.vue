208adb18f4f2e781a83a225c48a6866837e75cce
gitsjc711 <1090737321@qq.com>
2024-07-04 15:10:18 +0800

    增加了用户修改自己资料

--- src/views/student/user.vue
+++ src/views/student/user.vue
@@ -6,15 +6,34 @@
           :before-close="handleClose"
           width="50%">
         <!--表单数据-->
-        <el-form ref="form"  :rules="rules" :model="form" label-width="80px">
+        <el-form ref="form"  :model="form" label-width="80px">
           <el-form-item label="昵称" prop="nickname">
             <el-input v-model="form.nickname" placeholder="请输入昵称" style="width: 50%;"></el-input>
           </el-form-item>
-
-          
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="form.password" placeholder="请输入密码" style="width: 50%;"></el-input>
+          </el-form-item>
           <el-form-item label="邮箱" prop="email">
             <el-input v-model="form.email" placeholder="请输入邮箱" style="width: 50%;"></el-input>
           </el-form-item>
+          <el-form-item label="验证码" prop="code">
+            <el-input v-model="form.code" placeholder="请输入验证码" style="width: 50%;"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="sendEmail">发送验证码</el-button>
+          </el-form-item>
+          <el-form-item label="头像">
+          <el-upload
+            class="avatar-uploader"
+            :action="uploadUrl"
+            auto-upload="true"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item> 
         </el-form>
         <span slot="footer" class="dialog-footer">
           <el-button @click="cancel">取 消</el-button>
@@ -42,7 +61,7 @@
   </template>
 
 <script>
-import { mapState,mapMutations} from 'vuex';
+import { mapState,mapGetters} from 'vuex';
 export default {
   name: "HomeIfo",
   computed:{
@@ -56,21 +75,19 @@ export default {
           account:this.account,
           nickname:"",
           email:"",
-          role:""
+          role:"",
+          code:''
         },
         form:{
         nickname:'',
         email:'',
+        password:""
       },
-        dialogVisible: false,
-        rules: {
-          nickname: [
-            {required: true, message: '请输入昵称', trigger: 'blur'},
-          ],
-          email: [
-            {required: true, message: '请输入邮箱', trigger: 'blur'},
-          ],
-        },
+      dialogVisible: false,
+      uploadUrl:this.getBaseurl()+"/file/upload",
+      url:"" ,
+      imageUrl:""
+       
     }
 
   },
@@ -79,28 +96,58 @@ export default {
     this.getUserDetail()
   },
   methods:{
-    ...mapMutations(["updateRole","updateUid"]),
+    ...mapGetters(["getBaseurl"]),
     getUserDetail(){
-      this.$axios.post(this.baseUrl+"/user/showDetails",{
-      account:this.account,
-      },{  
-      headers: {  
-        'Content-Type': 'application/json'  
-      }}).then(
-        res=>{this.user=res.data
-        this.updateRole(this.user.role)
-        this.updateUid(this.user.id)
-      }
-      ).catch(error=>{console.error(error);})
+      return new Promise((resolve, reject) => {
+        this.$axios.post(this.baseUrl+"/user/showDetails",{
+          account:this.account,
+        },{  
+          headers: {  
+            'Content-Type': 'application/json'  
+          }}).then(
+            res=>{this.user=res.data
+              resolve(this.user)
+        }
+      ).catch(error=>{console.error(error);
+        reject(error)
+      })
+      })
     },
-    changeDetail(){
-     
+    
+    updateDetail(){
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+        if (this.form.email != ''&&(!re.test(String(this.form.email)))) {  
+          alert('请输入正确的邮箱地址');
+        } else {return new Promise((resolve, reject) => {
+        this.$axios.post(this.baseUrl+"/user/update",{
+          id:this.uid,
+          password:this.form.password,
+          nickname:this.form.nickname,
+          email:this.form.email,
+          code:this.form.code,
+          avatar:this.url
+        },{  
+          headers: {  
+            'Content-Type': 'application/json'  
+          }}).then(
+            res=>{this.errorCode=res.data
+              if(this.errorCode==="OK"){
+                alert("修改成功")
+              }else{
+                alert(this.errorCode)
+              }
+              resolve(this.errorCode)
+        }
+      ).catch(error=>{console.error(error);
+        reject(error)
+      })
+      })}
     },
     submit(){
         this.$refs.form.validate(async (valid)=>{
           if(valid){
-            await this.addChapter()
-            await this.findChapter()
+            await this.updateDetail()
+            await this.getUserDetail()
             console.log(this.form);
             // 关闭弹窗
             this.dialogVisible = false;
@@ -134,6 +181,45 @@ export default {
         this.modalType = 0;
         this.dialogVisible = true;
       }, 
+      sendEmail(){
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+        if (this.form.email === '') {  
+          alert("请输入邮箱地址") 
+        } else if (!re.test(String(this.form.email))) {  
+          alert('请输入正确的邮箱地址');  
+        } else {  
+          this.$axios.post(this.baseUrl+"/sendMail",{
+            email:this.form.email
+           },{  
+             headers: {  
+                'Content-Type': 'application/json'  
+            }}).then(res=>{this.errorCode=res.data
+              if(this.errorCode==="OK"){
+                alert("发送验证码成功")
+            }else{
+               alert(this.errorCode)  
+              }  
+           }
+           ).catch(error=>{console.error(error);}) 
+        }      
+      },
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+        this.url=res
+        alert("上传头像成功")
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      }
   }
 }
 </script>
@@ -194,5 +280,23 @@ img{
       margin-left: 60px;
     }
   }
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #525050;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
 }
 </style>
